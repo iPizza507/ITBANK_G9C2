b33429fe6b9a9cef59ae6582cdde0cb1b52aae22
iPizza <ianfigueroa507@gmail.com>
2022-05-31 11:08:17 -0300

    Validacion Form

--- Js/login.js
+++ Js/login.js
@@ -1,6 +1,7 @@
 //busco el input
 let email = document.getElementById("email");
 let contrasenia = document.getElementById("contrasenia");
+let nombre = document.getElementById("name");
 //validacion del formulario
 function validacionForm() {
   let em = email.value;
@@ -14,8 +15,12 @@ function validacionForm() {
     //cambia la ventana
     window.open("/home.html");
   }
+
+  let name = nombre.value;
+  localStorage.setItem("name", name);
 }
 function cargarDatos() {
-  let bienvenido = document.getElementById("bienvenido");
-  bienvenido.value = prompt("Ingrese un nombre");
+  let obtenerName = localStorage.getItem("name");
+  let bienvenidoText = document.getElementById("bienvenido");
+  bienvenidoText.innerText = "Bienvenid@ " + obtenerName + "!";
 }
