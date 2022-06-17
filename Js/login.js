1803b1ef0caa373a936ef58229051243aee50bca
iPizza <ianfigueroa507@gmail.com>
2022-06-17 12:02:34 -0300

    Arreglando la validacion de formulario Login

--- Js/login.js
+++ Js/login.js
@@ -1,19 +1,21 @@
 //busco el inputlet
 let email = document.getElementById("email");
-let contrasenia = document.getElementById("contrasenia");
+let contrasenia = document.getElementById("password");
 let nombre = document.getElementById("name");
 //validacion del formulario
 function validacionForm() {
   let em = email.value;
   let pass = contrasenia.value;
+  let name = nombre.value;
 
-  if (em === "" || pass === "") {
-    alert("Por favor ingrese usuario y/o contraseña");
+  if (em === "" || pass === "" || name === "") {
+    alert("Por favor ingrese nombre, usuario y/o contraseña");
   } else if (pass.length <= 6) {
-    alert("Tiene que tener un minimo de 6 caracteres");
+    alert("La contraseña debe tener un minimo de 6 caracteres");
+  } else if (name.length <= 4) {
+    alert("El nombre debe tener un minimo de 4 caracteres");
   } else {
     //cambia la ventana
-    let name = nombre.value;
     localStorage.setItem("name", name);
     alert("Load..");
     setTimeout(function () {
