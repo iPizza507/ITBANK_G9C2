1fcdb4177c4bbe2b651030f63b196b7e5214fcfe
vvintra <lucaslisandroarroyo02@gmail.com>
2022-07-17 19:11:17 -0300

    direccion

---
+++ Python/direccion.py
@@ -0,0 +1,26 @@
+class Direccion:
+    calle: str = ""
+    nombre: str = ""
+    ciudad: str = ""
+    provincia: str = ""
+    pais: str = ""
+    
+
+    def __init__(
+        self, calle: str, nombre: str, ciudad: str, provincia: str, pais: str
+    ) -> None:
+        self.calle = calle
+        self.nombre = nombre
+        self.ciudad = ciudad
+        self.provincia = provincia
+        self.pais = pais
+
+    def validate(
+        self,
+    ) -> bool:
+        return True
+
+    def outputAsLabel(
+        self,
+    ) -> str:
+        return ""
